c3f68773124bdecf6bb7a942ca7b2e3962d4cd58
Thorsten Glebe <thorsten.glebe@web.de>
2022-11-20 14:01:54 +0100

    enhanced TGScriptSkeleton, introduced OO Target View Control

--- TGScripts_repository/src/scripts/TGScripts/TGScriptSkeleton.js
+++ TGScripts_repository/src/scripts/TGScripts/TGScriptSkeleton.js
@@ -1,5 +1,5 @@
 /*
-   TGScriptSkeleton v1.2
+   TGScriptSkeleton v1.3
 
    A script for generating local support mask for deconvolution.
 
@@ -26,27 +26,22 @@
       added reset button
    v1.2
       added image selector preview
+   v1.3
+      replaced param GroupBox by image section
+      added documentation tool button
+      refactoring: introduce TargetViewSelector object
 */
 
-#include <pjsr/Sizer.jsh>
 #include <pjsr/FrameStyle.jsh>
 #include <pjsr/NumericControl.jsh>
 #include <pjsr/TextAlign.jsh>
 #include <pjsr/StdButton.jsh>
 #include <pjsr/StdIcon.jsh>
 
-#include "lib/TGScriptsLib.js"
+#include "lib/TGDialogLib.js"
 
-#define VERSION   "1.2"
+#define VERSION   "1.3"
 #define TITLE     "TGScriptSkeleton"
-
-// Largest dimension of the image preview in pixels.
-#define PREVIEW_SIZE 400
-#define SPACING      4
-#define MARGIN       8
-
-// basename
-#define BASENAME "TGScriptImg"
 
 #feature-id    TGScriptSkeleton : TG Scripts > TGScriptSkeleton
 
@@ -58,45 +53,11 @@
 // ----------------------------------------------------------------------------
 function ScriptData()
 {
-   this.dialog       = null;
    this.targetView   = null;
-   this.view_id      = "";
-   this.previewView  = null;
-   this.previewImage = null;
-   this.rgbLinked    = true;
 
-   // Save parameters in process icon
-   // -------------------------------------------------------------------------
-   this.exportParameters = function()
+   this.acquireDataFromDialog = function(dialog)
    {
-      Parameters.set("view_id"  , this.view_id);
-      Parameters.set("rgbLinked", this.rgbLinked);
-   }
-
-   // Restore saved parameters
-   // -------------------------------------------------------------------------
-   this.importParameters = function()
-   {
-      if (Parameters.has("view_id"))
-      {
-         this.view_id = Parameters.getString("view_id");
-         if(this.view_id)
-         {
-            var window = ImageWindow.windowById(this.view_id);
-            if(!window.isNull)
-            {
-               this.targetView = window.mainView;
-            }
-         }
-      }
-      if(Parameters.has("rgbLinked"))
-         this.rgbLinked = Parameters.getBoolean("rgbLinked");
-   }
-
-   this.reset = function()
-   {
-      this.targetView   = null;
-      this.view_id      = "";
+      this.targetView = dialog.targetViewSelector.getTargetView();
    }
 }
 
@@ -109,22 +70,21 @@ function doWork()
 {
    Console.show();
    var t0 = new Date;
-   data.targetView.beginProcess(UndoFlag_NoSwapFile);
 
    // Check if image is non-linear
-   if(data.targetView.image.median() > 0.01 && errorMessageOkCancel("Image seems to be non-linear, continue?", TITLE))
+   if(isStretched(data.targetView) && errorMessageOkCancel("Image seems to be non-linear, continue?", TITLE))
       return;
 
-   Console.writeln("targetView: ", data.view_id);
+   Console.writeln("targetView: ", data.targetView.id);
 
    //------ real work happens here ---------
 
    //------ real work end -----------------
 
-   data.targetView.endProcess();
    var t1 = new Date;
    Console.writeln(format("<end><cbr>doWork: %.2f s", (t1.getTime() - t0.getTime())/1000));
 }
+
 
 // ----------------------------------------------------------------------------
 // ScriptDialog
@@ -141,162 +101,35 @@ function ScriptDialog()
    this.helpLabel = new Label( this );
    with( this.helpLabel )
    {
-      frameStyle = FrameStyle_Box;
-      margin = 4;
+      frameStyle   = FrameStyle_Box;
+      margin       = MARGIN;
       wordWrapping = true;
-      useRichText = true;
+      useRichText  = true;
+      maxHeight    = 108; // fixed height
       text =
          "<p><b>" + TITLE + " v" + VERSION + "</b> &mdash; An empty script consisting "
-         + "of target view selection, new instance button to save state, ok and cancel button.</p>"
+         + "of target view selection with preview image including auto-stretch functionality. "
+         + "The buttons include the 'new instance' button to save the script state as an icon "
+         + "plus ok, cancel, reset and documentation tool button.</p>"
          + "<p>Copyright &copy; 2022 Thorsten Glebe</p>";
    }
 
+   // TargetViewSelector
    // -------------------------------------------------------------------------
-   this.ScrollControl = new Control( this );
+   this.targetViewSelector = new TargetViewSelector(this, "Target View Selection", labelWidth, "TargetViewSelectorImg");
 
-   this.ScrollControl.updateView = function()
-   {
-      var width, height;
-
-      if(data.targetView == null)
-      {
-         width  = 0;
-         height = 0;
-      }
-      else
-      {
-         var stf = data.targetView.stf;
-         if(isStretched(data.targetView))
-         {
-            data.previewImage = data.targetView.image;
-         }
-         else
-         {
-            if(hasSTF(data.targetView))
-            {
-               data.previewImage = applySTFHT(data.targetView.image, data.targetView.stf);
-            }
-            else
-            {
-               // copy view
-               data.previewView = copyView(data.targetView, uniqueViewId(BASENAME));
-               // STF stretch on copied view
-               ApplyAutoSTF(data.previewView, SHADOWS_CLIP, TARGET_BKG, data.rgbLinked);
-               data.previewImage = applySTFHT(data.previewView.image, data.previewView.stf);
-               data.previewView.window.forceClose();
-            }
-         }
-
-         var imageWidth  = data.previewImage.width;
-         var imageHeight = data.previewImage.height;
-         if ( imageWidth > imageHeight )
-         {
-            width  = PREVIEW_SIZE;
-            height = PREVIEW_SIZE*imageHeight/imageWidth;
-         }
-         else
-         {
-            width  = PREVIEW_SIZE*imageWidth/imageHeight;
-            height = PREVIEW_SIZE;
-         }
-      }
-
-      this.setFixedSize( width, height );
-      data.dialog.update();
-      data.dialog.adjustToContents();
-   }
-
-   this.ScrollControl.onPaint = function()
-   {
-      if(data.previewImage != null)
-      {
-         var G = new Graphics( this );
-         G.drawScaledBitmap( this.boundsRect, data.previewImage.render() );
-         G.end();
-      }
-      else
-      {
-         Console.writeln("targetView is null");
-      }
-   }
-
+   // exportParameters
    // -------------------------------------------------------------------------
-   this.targetImage_Label = new Label( this );
-   with( this.targetImage_Label )
+   this.exportParameters = function()
    {
-      minWidth = labelWidth;
-      text = "Target image:";
-      textAlignment = TextAlign_Right|TextAlign_VertCenter;
+      this.targetViewSelector.exportParameters();
    }
 
-   this.targetImage_ViewList = new ViewList( this );
-   with( this.targetImage_ViewList )
-   {
-      scaledMinWidth = 200;
-      getAll(); // include main views as well as previews
-      if ( data.targetView )
-      {
-         currentView = data.targetView;
-         data.view_id = currentView.id;
-      }
-
-      toolTip = this.targetImage_Label.toolTip = "Select the target image.";
-
-      onViewSelected = function( view )
-      {
-         if(view.id)
-         {
-            data.targetView = view;
-            data.view_id    = view.id;
-         }
-         else
-         {
-            data.targetView = null;
-            data.view_id = "";
-         }
-
-         data.dialog.linkedSTFCheckBox.updateCheckBox();
-         data.dialog.ScrollControl.updateView();
-      }
-   }
-
-   this.targetImage_Sizer = new HorizontalSizer;
-   with( this.targetImage_Sizer )
-   {
-      spacing = SPACING;
-      add( this.targetImage_Label );
-      add( this.targetImage_ViewList, 100 );
-   }
-
+   // importParameters
    // -------------------------------------------------------------------------
-   this.linkedSTFCheckBox = new CheckBox(this);
-   with( this.linkedSTFCheckBox )
+   this.importParameters = function()
    {
-      text    = "Use linked STF stretch";
-      toolTip = "<p>If set, linked STF stretch is used. Unset to stretch each channel individually.</p>";
-      checked = data.rgbLinked;
-
-      onClick = (checked) => { data.rgbLinked = checked; data.dialog.ScrollControl.updateView(); }
-   }
-
-   this.linkedSTFCheckBox.updateCheckBox = function()
-   {
-      if(data.targetView == null || data.targetView.image.isGrayscale || isStretched(data.targetView) || hasSTF(data.targetView))
-         this.enabled = false;
-      else
-         this.enabled = true;
-   }
-
-   // -------------------------------------------------------------------------
-   this.paramGroupBox = new GroupBox( this );
-   with( this.paramGroupBox )
-   {
-      title = "Target View Selection";
-      sizer = new VerticalSizer;
-      sizer.margin  = MARGIN;
-      sizer.spacing = SPACING;
-      sizer.add( this.targetImage_Sizer );
-      sizer.add( this.linkedSTFCheckBox );
+      this.targetViewSelector.importParameters();
    }
 
    // buttons
@@ -309,9 +142,7 @@ function ScriptDialog()
 
       onMousePress = function()
       {
-         this.hasFocus = true;
-         data.exportParameters();
-         this.pushed = false;
+         this.dialog.exportParameters();
          this.dialog.newInstance();
       }
    }
@@ -334,20 +165,24 @@ function ScriptDialog()
       onClick = () => { this.cancel(); }
    }
 
+   this.documentationButton = new ToolButton(this);
+   with( this.documentationButton )
+   {
+      icon = this.scaledResource( ":/process-interface/browse-documentation.png" );
+      toolTip = "<p>Show script documentaion.</p>";
+
+      onClick = () => { Dialog.browseScriptDocumentation( "TGScriptSkeleton" ); }
+   }
+
    this.reset_Button = new ToolButton(this);
    with( this.reset_Button )
    {
       icon = scaledResource( ":/process-interface/reset.png" );
       toolTip = "Reset to defaults";
 
-      onMousePress = function()
+      onMousePress = () =>
       {
-         if(data.dialog.targetImage_ViewList.currentView)
-         {
-            data.dialog.targetImage_ViewList.remove(data.dialog.targetImage_ViewList.currentView);
-            data.dialog.targetImage_ViewList.getAll();
-         }
-         data.reset();
+         this.targetViewSelector.resetControl();
       }
    }
 
@@ -359,6 +194,7 @@ function ScriptDialog()
       addStretch();
       add(this.ok_Button);
       add(this.cancel_Button);
+      add(this.documentationButton);
       add(this.reset_Button);
    }
 
@@ -371,16 +207,14 @@ function ScriptDialog()
       spacing = SPACING;
       add( this.helpLabel );
       addSpacing( SPACING );
-      add( this.ScrollControl, 100 );
-      addSpacing( SPACING );
-      add( this.paramGroupBox );
+      add( this.targetViewSelector );
       addSpacing( SPACING );
       add( this.buttons_Sizer );
    }
 
    this.windowTitle = TITLE;
-   this.setScaledFixedWidth(PREVIEW_SIZE + 2*MARGIN);
-   //this.setScaledMinSize(600, 400);
+   this.setFixedWidth(PREVIEW_SIZE + 2*MARGIN);
+//   this.setScaledMinSize(PREVIEW_SIZE, PREVIEW_SIZE);
    // this.setFixedSize(); // this prevents main window to scale
    this.adjustToContents();
 } // ScriptDialog
@@ -393,40 +227,26 @@ ScriptDialog.prototype = new Dialog;
 // ----------------------------------------------------------------------------
 function main()
 {
+   var dialog = new ScriptDialog();
+
    Console.hide();
+   dialog.importParameters();
    if (Parameters.isViewTarget)
    {
-      data.importParameters();
-      data.targetView = Parameters.targetView;
-      data.view_id    = Parameters.targetView.id;
       doWork();
       return;
    }
-   else
-   {
-      data.importParameters();
-      if(!data.targetView)
-      {
-         // Get access to the active image window
-         var window = ImageWindow.activeWindow;
-         if (!window.isNull)
-         {
-            data.targetView = window.currentView;
-            data.view_id    = window.currentView.id;
-         }
-      }
-   }
-
-   var dialog = new ScriptDialog();
 
    // initialize dialog
-   dialog.linkedSTFCheckBox.updateCheckBox();
-   dialog.ScrollControl.updateView();
+   dialog.targetViewSelector.updateSelectorView();
+
    //   dialog.userResizable = false;
    for ( ;; )
    {
       if ( !dialog.execute() )
          return;
+
+      data.acquireDataFromDialog(dialog);
 
       // A view must be selected.
       if ( !data.targetView )
---
+++ TGScripts_repository/src/scripts/TGScripts/lib/TGDialogLib.js
@@ -0,0 +1,299 @@
+/*
+   TGDialogLib v1.0
+
+   A script containing common methods for the TGScripts script family.
+
+   Copyright (C) 2022 Dr. Thorsten Glebe
+
+   This program is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation, version 3 of the License.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+   more details.
+
+   You should have received a copy of the GNU General Public License along with
+   this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <pjsr/Sizer.jsh>
+#include <pjsr/SectionBar.jsh>
+
+#include "TGScriptsLib.js"
+
+#define PREVIEW_SIZE 400
+#define SPACING      4
+#define MARGIN       8
+
+// ----------------------------------------------------------------------------
+// TargetViewSelector
+// ----------------------------------------------------------------------------
+function TargetViewSelector(dialog, selector_title, labelWidth, basename)
+{
+   this.__base__ = VerticalSizer;
+  	this.__base__();
+
+   // member variables
+   var targetView   = null;
+   var view_id      = "";
+   var previewView  = null;
+   var previewImage = null;
+   var rgbLinked    = false;
+
+   // getter for targetView
+   // -------------------------------------------------------------------------
+   this.getTargetView = function()
+   {
+      return targetView;
+   }
+
+   // Save parameters in process icon
+   // -------------------------------------------------------------------------
+   this.exportParameters = function()
+   {
+      Parameters.set("view_id"  , view_id);
+      Parameters.set("rgbLinked", rgbLinked);
+   }
+
+   // Restore saved parameters
+   // -------------------------------------------------------------------------
+   this.importParameters = function()
+   {
+      if (Parameters.isViewTarget)
+      {
+         targetView = Parameters.targetView;
+         view_id    = Parameters.targetView.id;
+      }
+      else
+      {
+         if (Parameters.has("view_id"))
+         {
+            view_id = Parameters.getString("view_id");
+            if(view_id)
+            {
+               var window = ImageWindow.windowById(view_id);
+               if(!window.isNull)
+               {
+                  targetView = window.mainView;
+               }
+            }
+         }
+         else
+         {
+            // Get access to the active image window
+            var window = ImageWindow.activeWindow;
+            if (!window.isNull)
+            {
+               targetView = window.currentView;
+               view_id    = window.currentView.id;
+            }
+         }
+      }
+      if(Parameters.has("rgbLinked"))
+         rgbLinked = Parameters.getBoolean("rgbLinked");
+
+      // update ViewList control
+      this.targetImage_ViewList.currentView = targetView;
+   }
+
+   // -------------------------------------------------------------------------
+   this.ScrollControl = new Control( dialog );
+   this.ScrollControl.updateView = function()
+   {
+      var width, height;
+
+      if(targetView == null)
+      {
+         width  = 0;
+         height = 0;
+      }
+      else
+      {
+         var stf = targetView.stf;
+         if(isStretched(targetView))
+         {
+            previewImage = targetView.image;
+         }
+         else
+         {
+            if(hasSTF(targetView))
+            {
+               previewImage = applySTFHT(targetView.image, targetView.stf);
+            }
+            else
+            {
+               // copy view
+               previewView = copyView(targetView, uniqueViewId(basename));
+               // STF stretch on copied view
+               ApplyAutoSTF(previewView, SHADOWS_CLIP, TARGET_BKG, rgbLinked);
+               previewImage = applySTFHT(previewView.image, previewView.stf);
+               previewView.window.forceClose();
+            }
+         }
+
+         var imageWidth  = previewImage.width;
+         var imageHeight = previewImage.height;
+         if ( imageWidth > imageHeight )
+         {
+            width  = PREVIEW_SIZE;
+            height = PREVIEW_SIZE*imageHeight/imageWidth;
+         }
+         else
+         {
+            width  = PREVIEW_SIZE*imageWidth/imageHeight;
+            height = PREVIEW_SIZE;
+         }
+      }
+
+      this.setFixedSize( width, height );
+      dialog.adjustToContents();
+   }
+
+   this.ScrollControl.onPaint = function()
+   {
+      if(previewImage != null)
+      {
+         var G = new Graphics( this );
+         G.drawScaledBitmap( this.boundsRect, previewImage.render() );
+         G.end();
+      }
+      else
+      {
+         Console.writeln("targetView is null");
+      }
+   }
+
+   // -------------------------------------------------------------------------
+   this.targetImage_Label = new Label( dialog );
+   with( this.targetImage_Label )
+   {
+      minWidth = labelWidth;
+      text = "Target image:";
+      textAlignment = TextAlign_Right|TextAlign_VertCenter;
+   }
+
+   this.targetImage_ViewList = new ViewList( dialog );
+   with( this.targetImage_ViewList )
+   {
+      getAll(); // include main views as well as previews
+
+      toolTip = this.targetImage_Label.toolTip = "Select the target image.";
+
+      onViewSelected = (view) =>
+      {
+         if(view.id)
+         {
+            targetView = view;
+            view_id    = view.id;
+         }
+         else
+         {
+            targetView = null;
+            view_id    = "";
+         }
+
+         this.updateSelectorView();
+      }
+   }
+
+   this.targetImage_Sizer = new HorizontalSizer;
+   with( this.targetImage_Sizer )
+   {
+      spacing = SPACING;
+      add( this.targetImage_Label );
+      add( this.targetImage_ViewList );
+   }
+
+   // -------------------------------------------------------------------------
+   this.linkedSTFCheckBox = new CheckBox(dialog);
+   with( this.linkedSTFCheckBox )
+   {
+      text    = "Use linked STF stretch";
+      toolTip = "<p>If set, linked STF stretch is used. Unset to stretch each channel individually.</p>";
+      checked = rgbLinked;
+
+      onClick = (checked) => { rgbLinked = checked; dialog.ScrollControl.updateView(); }
+   }
+
+   this.linkedSTFCheckBox.updateCheckBox = function()
+   {
+      if(targetView == null || targetView.image.isGrayscale || isStretched(targetView) || hasSTF(targetView))
+         this.enabled = false;
+      else
+         this.enabled = true;
+   }
+
+   // -------------------------------------------------------------------------
+   this.paramVSizer = new VerticalSizer( dialog );
+   with( this.paramVSizer )
+   {
+      margin  = MARGIN;
+      spacing = SPACING;
+      add( this.targetImage_Sizer );
+      addSpacing( SPACING );
+      add( this.linkedSTFCheckBox );
+   }
+
+   // -------------------------------------------------------------------------
+   this.imageSection = new Control(dialog);
+   with (this.imageSection)
+   {
+      sizer = new HorizontalSizer;
+      with (sizer)
+      {
+         spacing = SPACING;
+         this.imageSectionVSizer = new VerticalSizer;
+         with (this.imageSectionVSizer)
+         {
+            margin  = MARGIN;
+            spacing = SPACING;
+            add( this.paramVSizer );
+            addSpacing( SPACING );
+            add(this.ScrollControl);
+         }
+         add(this.imageSectionVSizer);
+      }
+   }
+
+   // -------------------------------------------------------------------------
+   this.imageSectionBar = new SectionBar(dialog, selector_title);
+   with(this.imageSectionBar)
+   {
+      setSection(this.imageSection);
+   }
+
+   // -------------------------------------------------------------------------
+   this.updateSelectorView = function()
+   {
+      this.linkedSTFCheckBox.updateCheckBox();
+      this.ScrollControl.updateView();
+      dialog.adjustToContents();
+   }
+
+   // -------------------------------------------------------------------------
+   this.resetControl = function()
+   {
+      if(this.targetImage_ViewList.currentView)
+      {
+         this.targetImage_ViewList.remove(this.targetImage_ViewList.currentView);
+         this.targetImage_ViewList.getAll();
+      }
+
+      targetView   = null;
+      view_id      = "";
+      previewView  = null;
+      previewImage = null;
+      rgbLinked    = false;
+   }
+
+   // register elements to this sizer
+   margin  = MARGIN;
+   spacing = SPACING;
+   this.add( this.imageSectionBar );
+   this.add( this.imageSection );
+
+} // TargetViewSelector
+
+TargetViewSelector.prototype = new VerticalSizer();
